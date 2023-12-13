3c8991148cd18ab2d6d305c64f0e20a0b648b507
Md. Hasnat Shoheb <hasnat.shoheb@selise.ch>
2023-12-13 18:06:06 +0600

    Assignment-3

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -1,9 +1,7 @@
 const { expect } = require('@wdio/globals')
 const LoginPage = require('../pageobjects/login.page')
 const SecurePage = require('../pageobjects/secure.page')
-// const HomePage = require('../pageobjects/home.page')
-// const randomNumber = Math.floor(Math.random() * 1000);
-// const zone  = 'ZoneTest'+ randomNumber;
+
 
 describe('Hawle application', () => {
     it('should login with valid credentials', async () => {
@@ -11,6 +9,7 @@ describe('Hawle application', () => {
         await LoginPage.login('ntypeuser@yopmail.com', '55995960')
         await expect(SecurePage.flashAlert).toBeExisting();
         await expect(SecurePage.flashAlert).toHaveTextContaining('home');
+        console.log("Test Passed")
     })
 
 })
